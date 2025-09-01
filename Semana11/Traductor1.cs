9b39b74b9be8288e02e279cfca69084a281c3f47
Tu Nombre <tu_correo@example.com>
2025-08-31 19:21:32 -0500

    Inicializacion del diccionario Base español -- ingles

--- Semana11/Traductor1.cs
+++ Semana11/Traductor1.cs
@@ -2,7 +2,31 @@
 {
     public static void Main()
     {
+        //Diccionario inicial de español ----> Ingles
+        Dictionary<string, string> diccionario = new Dictionary<string, string>()
+        {
+            { "tiempo", "time" },
+            { "persona", "person" },
+            { "ano", "year" },
+            { "camino", "way" },
+            { "dia", "day" },
+            { "cosa", "thing" },
+            { "hombre", "man" },
+            { "mundo", "world" },
+            { "vida", "life" },
+            { "mano", "hand" },
+            { "parte", "part" },
+            { "nino", "child" },
+            { "ojo", "eye" },
+            { "mujer", "woman" },
+            { "lugar", "place" },
+            { "trabajo", "work" },
+            { "semana", "week" },
+            { "caso", "case" },
+            { "punto", "point" },
+            { "gobierno", "government" },
+            { "empresa", "company" }
+        };
 
-        
     }
 }
